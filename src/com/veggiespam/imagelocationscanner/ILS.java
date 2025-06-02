355305693cba20ebef8949dd7c646fafcaec408c
veggiespam <3471999+veggiespam@users.noreply.github.com>
2025-06-02 11:36:24 -0400

    un-need close() fails ZAP's build.

--- src/com/veggiespam/imagelocationscanner/ILS.java
+++ src/com/veggiespam/imagelocationscanner/ILS.java
@@ -980,7 +980,9 @@ public class ILS {
 				if (read != size) {
 				    System.out.println("There was a problem reading the file");
 				}
-				fis.close();
+				// ZAP build fail using -Werror with msg:
+				// `warning: [try] explicit call to close() on an auto-closeable resource`
+ 				//fis.close();
 				
 				String res = scanForLocationInImage(data, outputtype);
 				if (0 == res.length())  {
